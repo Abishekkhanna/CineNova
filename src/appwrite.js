eef00e2597f7f3c9bab1ae535899dee8eec9b52d
Abishek Khanna M S <75411526+Abishekkhanna@users.noreply.github.com>
2025-11-18 14:21:08 +0530

    Connected DB with appwrite and updated the count of every search

---
+++ src/appwrite.js
@@ -0,0 +1,37 @@
+import { Client, Databases, Query, ID } from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_NAME;
+const END_POINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+
+const client = new Client().setEndpoint(END_POINT).setProject(PROJECT_ID);
+const database = new Databases(client);
+
+export const updateSearchTerm = async (searchTerm, movie) => {
+  // 1. use appwrite to check the searchTerm exist in DB
+  try {
+    const res = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
+
+    // 2. If available then update it
+    if (res.documents.length > 0) {
+      const doc = res.documents[0];
+
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+    } else {
+      // 3. If it doesn't, create a new document with the searchTerm and count as 1
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_url}`,
+      });
+    }
+  } catch (error) {
+    console.error(`Error updating search term in appwrite ${error}`);
+  }
+};
